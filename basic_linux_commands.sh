fa8d2f650fd9fa3ecd5b14d3e7e432dedacc13f6
Md. Ashraful Alam <77880159+ashraful005@users.noreply.github.com>
2025-01-23 13:19:30 +0600

    date and pwd

--- basic_linux_commands.sh
+++ basic_linux_commands.sh
@@ -1,3 +1,10 @@
 #Change directory
 cd ~ 	#Goes to home directory
 cd - 	#Goes to previous directory
+
+#Date and time
+date 	#Default date and time
+date -u #Date and time with respect to UTC
+
+#Directory
+pwd	#Present working directory
