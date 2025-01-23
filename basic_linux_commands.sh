33bae8511d8562618e5a9768f89f2a5f91214143
Md. Ashraful Alam <77880159+ashraful005@users.noreply.github.com>
2025-01-23 13:30:30 +0600

    which, whereis, w commands

--- basic_linux_commands.sh
+++ basic_linux_commands.sh
@@ -1,10 +1,18 @@
 #Change directory
-cd ~ 	#Goes to home directory
-cd - 	#Goes to previous directory
+cd ~ 		#Goes to home directory
+cd - 		#Goes to previous directory
 
 #Date and time
-date 	#Default date and time
-date -u #Date and time with respect to UTC
+date 		#Default date and time
+date -u 	#Date and time with respect to UTC
 
 #Directory
-pwd	#Present working directory
+pwd		#Present working directory
+pwd -L  	#Logical path of present working directory
+pwd -P  	#Physical path of present working directory
+
+w 		#Shows the users logged-in 
+which pwd 	#Shows the pwd path
+whereis pwd	#Shows both binary and documentation path of pwd
+whereis -b pwd	#Shows only the binary pwd path
+whereis -m pwd	#shows the documentation or man page path of pwd
